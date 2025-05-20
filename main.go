f8768f444914ec01c114b8a4b6bdf35f4086618c
loywizzy <kdftvguitarandarmy@gmail.com>
2025-05-20 10:18:22 +0700

    Fix merge error

--- main.go
+++ main.go
@@ -5,54 +5,35 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
-	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/joho/godotenv"
+
 	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-	// เริ่มต้นการตั้งค่า log
-	log.SetFormatter(&log.TextFormatter{
-		FullTimestamp: true,
-	})
-	log.Info("Starting API server...")
+	err := godotenv.Load()
 
-	// สร้าง Fiber app
-	app := fiber.New(fiber.Config{
-		// เพิ่มการตั้งค่าตามที่ต้องการ
-		AppName:      "Payso Internal API",
-		ErrorHandler: customErrorHandler,
-	})
-
-	// เพิ่ม middleware
-	app.Use(cors.New())
-	app.Use(logger.New())
-
-	// ตั้งค่า routes
-	router.SetupRoutes(app)
-
-	// เพิ่ม simple root endpoint
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Payso Internal API is running")
-	})
-
-	// เริ่มการรัน server
-	log.Info("Server is running on port 8080")
-	log.Fatal(app.Listen(":8080"))
-}
-
-// Custom error handler
-func customErrorHandler(c *fiber.Ctx, err error) error {
-	// ส่ง default error 500 กลับไป
-	code := fiber.StatusInternalServerError
-
-	// Override status code if known error type
-	if e, ok := err.(*fiber.Error); ok {
-		code = e.Code
+	if err != nil {
+		log.Fatal("Error loading .env file")
 	}
 
-	return c.Status(code).JSON(fiber.Map{
-		"status":  "error",
-		"message": err.Error(),
-		"data":    nil,
-	})
+	port := "8080"
+
+	app := fiber.New()
+	app.Use(cors.New())
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+	}))
+
+	app.Use(cors.New())
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+	}))
+
+	log.Info("==-- Start Internal Service --==")
+	router.SetupRoutes(app)
+
+	app.Listen(":" + port)
+
 }
